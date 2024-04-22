dc1a07ef08c117354b4e125fc1e0ac8c7db276c2
arevalo8 <javi8arevalo@gmail.com>
2024-04-22 19:58:33 +0200

    Arreglado

--- docs/main.tf
+++ docs/main.tf
@@ -1,25 +1,18 @@
-# Configuración del proveedor Docker
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host = "tcp://localhost:2375/"
 }
 
-# Recurso para construir la imagen de Docker
 resource "docker_image" "jenkins" {
-  name = "my-jenkins"
-
-  build {
-    context    = "./"
-    dockerfile = "Dockerfile"
-  }
+  name = "your-custom-jenkins-image:latest"
+  keep_locally = false
 }
 
-# Recurso para crear el contenedor Docker a partir de la imagen
 resource "docker_container" "jenkins" {
-  image = docker_image.jenkins.name
-  name  = "jenkins-container"
-
+  name = "jenkins-container"
+  image = docker_image.jenkins.latest
   ports {
     internal = 8080
     external = 8080
   }
+  restart = "always"
 }
