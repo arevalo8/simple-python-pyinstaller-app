d3e8c34cd1768896c5c245295925eacbada033a6
arevalo8 <javi8arevalo@gmail.com>
2024-04-22 14:12:44 +0200

    Arreglados Dockerfile y Terraform files 2

--- docs/main.tf
+++ docs/main.tf
@@ -3,17 +3,21 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# Recurso para construir la imagen de Docker
 resource "docker_image" "jenkins" {
-  name   = "my-jenkins"
+  name = "my-jenkins"
+
   build {
     context    = "./"
     dockerfile = "Dockerfile"
   }
 }
 
+# Recurso para crear el contenedor Docker a partir de la imagen
 resource "docker_container" "jenkins" {
-  image = docker_image.jenkins.name  # Utiliza el atributo "name"
+  image = docker_image.jenkins.name
   name  = "jenkins-container"
+
   ports {
     internal = 8080
     external = 8080
